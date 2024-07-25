d86fcafb09d627e8f717be8990be6d5c8e36fc08
Fernando Fontes <fernando.fontes@gomind.com.br>
2024-07-25 15:37:35 -0300

    fix: the entry_point

--- lib/scripts/cli.py
+++ lib/scripts/cli.py
@@ -1,10 +1,5 @@
 import argparse
 
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-
 from db2excel.postgresql import PostgreSQLToExcel
 from db2excel.sqlite     import SqliteToExcel
 
