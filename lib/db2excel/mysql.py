2ad347907a5823a341b37d65c2705d14ceaec984
Fernando Fontes <fernando.fontes@gomind.com.br>
2024-07-29 09:13:12 -0300

    feat: creating new method for mysql users

---
+++ lib/db2excel/mysql.py
@@ -0,0 +1,44 @@
+from ._base import DatabaseToExcel
+from typing import Union
+
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine
+
+class MySQLToExcel(DatabaseToExcel):
+    
+    def __init__(
+        self,
+        database         : str,
+        username         : str,
+        password         : str,
+        host             : str,
+        port             : Union[int, str],
+        download_path    : str,
+        excel_name       : str  = None,
+        overwrite        : bool = False,
+    ):
+        self.database = database
+        self.username = username
+        self.password = password
+        self.host     =     host
+        self.port     =     port
+        
+        super().__init__(
+            download_path = download_path,
+            excel_name    =    excel_name,
+            overwrite     =     overwrite,
+        )
+        
+    
+    def creating_engine(self) -> Engine:
+        db_url = {
+            'database'  : self.database,
+            'drivername': 'mysql+pymysql',
+            'username'  : self.username,
+            'password'  : self.password,
+            'host'      : self.host,
+            'port'      : self.port,
+        }
+        return create_engine(URL.create(**db_url))
+        
